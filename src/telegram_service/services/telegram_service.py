5f2cb1fe64e8a4c4f5315eaf5ad0bb4e54fab34f
idevtier <idevtier@gmail.com>
2022-06-18 19:59:09 +0300

    Refactoring

--- src/telegram_service/services/telegram_service.py
+++ src/telegram_service/services/telegram_service.py
@@ -4,6 +4,7 @@ from typing import Protocol
 from telegram_service.domain.dtos import Event, MessageRequest
 from telegram_service.services.client_service import ClientService
 from telegram_service.services.notification_service import NotificationService
+from telegram_service.config import settings
 
 
 class TelegramService(Protocol):
@@ -16,15 +17,16 @@ class AiogramTelegramService:
         self,
         notification_service: NotificationService,
         client_service: ClientService,
-        snooze_time: float = 0.3
+        push_pause: float = settings.push_pause
     ) -> None:
 
         self._notification_service: NotificationService = notification_service
         self._client_service: ClientService = client_service
-        self._snooze_time: float = snooze_time
+        self._snooze_time: float = push_pause
 
     async def send_event(self, event: Event) -> None:
-        for client in await self._client_service.get_all_clients():
+        clients_generator = await self._client_service.get_all_clients()
+        for client in clients_generator:
             await self._notification_service.send_photo(
                 client.telegram_id, event.media, event.text)
             await asyncio.sleep(self._snooze_time)
