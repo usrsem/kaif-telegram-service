e8e1dd16a0ebb88da25b216e5c836237a9e20de8
idevtier <idevtier@gmail.com>
2022-06-18 19:59:40 +0300

    Updating handlers package

--- src/telegram_service/handlers/start_handler.py
+++ src/telegram_service/handlers/start_handler.py
@@ -1,19 +1,31 @@
-from telegram_service.clients.client_service import ClientService
+import grpc
 import telegram_service.factories as factories
-from telegram_service.domain.dtos import Client
-from telegram_service.loader import dp
 from aiogram.types import Message
+from telegram_service.services.client_service import ClientService
+from telegram_service.domain.dtos import Client
+from telegram_service.loader import dp, log
 
 
 @dp.message_handler(commands=["start"])
-async def start_handler(message: Message) -> Message:
+async def start_handler(message: Message) -> None:
+    log.debug(f"Getting start {message=}")
+
     client: Client = Client(
         telegram_id=message.from_user.id,
         telegram_username=message.from_user.username,
         telegram_fullname=message.from_user.full_name)
 
     service: ClientService = factories.get_client_service()
-    await service.add_client(client)
 
-    return Message(chat_id=client.telegram_id, text="Hello")
+    # FIXME: Start sending exception about created client
+    # from server, don't catch UNKNOWN
+    try:
+        await service.add_client(client)
+        await message.reply(text="Hello")
+    except grpc.RpcError as e:
+        if e.code() == grpc.StatusCode.UNKNOWN: # type: ignore
+            log.warning(f"Client already in db, {e=}")
+        else:
+            raise e
+
 
