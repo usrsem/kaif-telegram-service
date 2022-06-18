dd03fb2a7f74e57a7b5639012083eb016f77181e
idevtier <idevtier@gmail.com>
2022-06-18 19:58:26 +0300

    Refactoring

--- src/telegram_service/config.py
+++ src/telegram_service/config.py
@@ -3,9 +3,12 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    bot_token: str = os.getenv("KAIF_TELEGRAM_SERVICE_BOT_TOKEN", "")
     clients_service_host: str = os.getenv("KAIF_CLIENT_SERVICE_ADDRESS", "")
-    telegram_service_port: str = os.getenv("KAIF_TELEGRAM_SERVICE_PORT", "")
+    bot_token: str = os.getenv("KAIF_TELEGRAM_SERVICE_BOT_TOKEN", "")
+    telegram_service_port: int = int(os.getenv(
+                                         "KAIF_TELEGRAM_SERVICE_PORT", ""))
+    push_pause: float = float(os.getenv(
+                                  "KAIF_TELEGRAM_SERVICE_PUSH_PAUSE", "0.3"))
 
 
 settings = Settings()
